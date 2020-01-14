e9b71d7c8e15fe473fc415102eafb0ac91fb7ea1
anup9819 <47321609+anup9819@users.noreply.github.com>
2020-01-14 15:04:13 +0530

    jumpman game
    tap onscreen to make the character jump as higher as you can to collect coins and dodge the incoming bombs

---
+++ android/src/com/jumpman/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jumpman.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jumpman.game.JumpMan;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new JumpMan(), config);
+	}
+}
